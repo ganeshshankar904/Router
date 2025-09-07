1e432a175e233fec880dd78dfa41ab9e9f036750
ganeshshankar904 <GaneshShankar904@gmail.com>
2025-09-07 16:00:30 +0530

    Initial Commit

---
+++ Router/RouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterApp.swift
+//  Router
+//
+//  Created by ganesh shankar on 07/09/25.
+//
+
+import SwiftUI
+
+@main
+struct RouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
